c97523dea51e2069eaa828f10c035cc8a1a12792
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-21 23:34:09 +0530

    displayed gray and blur images

--- document_scanner.py
+++ document_scanner.py
@@ -4,6 +4,10 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur_img = cv2.GaussianBlur(gray_img, (5, 5), 1)
 
     cv2.imshow("Image", img)
+    cv2.imshow("Gray Image", gray_img)
+    cv2.imshow("Blur Image", blur_img)
     cv2.waitKey(1)
