28146e83d8131a03cda35a1d9ea5ef054251ece3
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-21 23:58:26 +0530

    from my_utils imported preProcessing function, set image width and height

--- document_scanner.py
+++ document_scanner.py
@@ -1,4 +1,5 @@
 import cv2
+from my_utils import preProcessing
 
 ###############################
 width_img = 640
@@ -13,16 +14,7 @@ cap.set(4, height_frame)
 
 while True:
     success, img = cap.read()
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur_img = cv2.GaussianBlur(gray_img, (5, 5), 1)
-    canny_img = cv2.Canny(blur_img, 50, 50)
-    dial_img = cv2.dilate(canny_img, (5, 5), iterations=2)
-    erode_img = cv2.erode(dial_img, (5, 5), iterations=1)
-
-    cv2.imshow("Image", img)
-    cv2.imshow("Gray Image", gray_img)
-    cv2.imshow("Blur Image", blur_img)
-    cv2.imshow("Canny Image", canny_img)
-    cv2.imshow("Dilate Image", dial_img)
-    cv2.imshow("Erode Image", erode_img)
+    cv2.resize(img, (width_img, height_img))
+    output_img = preProcessing(img)
+    cv2.imshow("Image", output_img)
     cv2.waitKey(1)
