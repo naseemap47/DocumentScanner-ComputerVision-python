25766a3e6e866f03c2a190a990f5f73b4dcf9dc7
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-24 00:05:26 +0530

    readed sample document image

--- document_scanner.py
+++ document_scanner.py
@@ -8,19 +8,22 @@ width_frame = 640
 height_frame = 480
 ###############################
 
-cap = cv2.VideoCapture(0)
-cap.set(3, width_frame)
-cap.set(4, height_frame)
+doc_photo = cv2.imread('document sample.jpeg')
+
+
+# cap = cv2.VideoCapture(0)
+# cap.set(3, width_frame)
+# cap.set(4, height_frame)
 
 while True:
-    success, img = cap.read()
-    cv2.resize(img, (width_img, height_img))
-    canny_img = preProcessing(img)
-    large_approx = getContour(canny_img, img)
-    doc_img = getWarp(img, large_approx, width_img, height_img)
-    if len(large_approx) != 0:
-        print(len(large_approx))
-    cv2.imshow("Image", img)
-    cv2.imshow("Canny Image", canny_img)
-    cv2.imshow("Output Image", doc_img)
+    # success, img = cap.read()
+    # cv2.resize(img, (width_img, height_img))
+    # canny_img = preProcessing(img)
+    # large_approx = getContour(canny_img, img)
+    # doc_img = getWarp(img, large_approx, width_img, height_img)
+    # if len(large_approx) != 0:
+    #     print(len(large_approx))
+    # cv2.imshow("Image", img)
+    # cv2.imshow("Canny Image", canny_img)
+    cv2.imshow("Output Image", doc_photo)
     cv2.waitKey(1)
