c46c117435b785534d9b161c495d80552ddf8cd4
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-24 18:39:48 +0530

    Removed edge points on the scanned Document

--- document_scanner.py
+++ document_scanner.py
@@ -11,15 +11,17 @@ height_frame = 480
 # Image Scanner
 image_scanner = True
 if image_scanner:
-    doc_img = cv2.imread('sample4.jpg')
+    doc_img = cv2.imread('sample9.jpeg')
     cv2.resize(doc_img, (width_img, height_img))
+    doc_img_copy = doc_img.copy()
+    cv2.resize(doc_img_copy, (width_img, height_img))
     canny_img = preProcessing(doc_img)
     large_approx = getContour(canny_img, doc_img)
     # print(large_approx)
     if len(large_approx) != 0:
         # print(len(large_approx))
         ordered_points = reOrder(large_approx)
-        output_img = getWarp(doc_img, ordered_points, width_img, height_img)
+        output_img = getWarp(doc_img_copy, ordered_points, width_img, height_img)
     else:
         output_img = doc_img
         cv2.putText(
